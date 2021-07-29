160daf2fb4e59b817202f103daa453ecad8e9ac2
evandro <evandro.alves@farme.com.br>
2021-07-29 11:59:01 -0300

    refactor: organizing files by folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './cities/controller/app.controller';
+import { AppService } from './cities/service/app.service';
 
 @Module({
   imports: [],
